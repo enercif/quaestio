5f1b012ae4909e8199630f01d599c03e759ec6b5
Esra Akay <esra.akay67@icloud.com>
2026-07-09 05:45:05 +0200

    Add access protection to teachers route

---
+++ src/routes/teacher/+layout.server.ts
@@ -0,0 +1,12 @@
+import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
+
+export const load: LayoutServerLoad = async ({ locals }) => {
+	if (!locals.user || locals.user.role != 'user') {
+		throw redirect(303, '/');
+	}
+
+	return {
+		user: locals.user
+	};
+};
