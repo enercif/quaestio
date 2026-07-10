9741ee50b0336d36f5300ccd49876c2e0399b9a0
Esra Akay <esra.akay67@icloud.com>
2026-07-10 02:52:24 +0200

    Add title

--- src/routes/teacher/settings/password/+page.svelte
+++ src/routes/teacher/settings/password/+page.svelte
@@ -28,30 +28,34 @@
 	}
 </script>
 
-<form onsubmit={changePassword} class="mt-10 flex w-lg flex-col gap-6">
-	<div>
-		<Label for="current-password" class="mb-2 text-sm font-medium">Current password</Label>
-		<Input
-			id="current-password"
-			type="password"
-			bind:value={currentPassword}
-			required
-			placeholder="Current password"
-		></Input>
-	</div>
-	<div>
-		<Label for="new-password" class="mb-2 text-sm font-medium">New password</Label>
-		<Input
-			id="new-password"
-			type="password"
-			bind:value={newPassword}
-			required
-			placeholder="New password"
-		></Input>
-		{#if error}<p class="text-sm text-red-500">
-				{error}
-			</p>{/if}
-	</div>
-	<Button class="size-lg w-full" type="submit">Submit</Button>
-</form>
+<div class="flex size-full flex-col items-center justify-center gap-4">
+	<h1 class="mt-10 w-full max-w-lg text-2xl font-semibold">Passwort ändern</h1>
+
+	<form onsubmit={changePassword} class="flex w-full max-w-lg flex-col gap-6">
+		<div>
+			<Label for="current-password" class="mb-2 text-sm font-medium">Aktuelles passwort</Label>
+			<Input
+				id="current-password"
+				type="password"
+				bind:value={currentPassword}
+				required
+				placeholder="Current password"
+			></Input>
+		</div>
+		<div>
+			<Label for="new-password" class="mb-2 text-sm font-medium">Neues passwort</Label>
+			<Input
+				id="new-password"
+				type="password"
+				bind:value={newPassword}
+				required
+				placeholder="New password"
+			></Input>
+			{#if error}<p class="text-sm text-red-500">
+					{error}
+				</p>{/if}
+		</div>
+		<Button class="size-lg w-full" type="submit">Submit</Button>
+	</form>
+</div>
 <Button href="/teacher/quizzes" variant="link">Exit</Button>
